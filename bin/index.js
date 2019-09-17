f3f7defc1945ef9a9cb40b970832d220c530baf4
zbo14 <zbalder@starry.com>
2019-09-17 13:41:52 -0400

    get cookies from process.env

--- bin/index.js
+++ bin/index.js
@@ -6,11 +6,12 @@ const run = require('./run')
 
 const url = process.argv[2]
 const batchSize = +process.env.BATCH_SIZE
+const cookies = process.env.COOKIES
 const headers = process.env.HEADERS
 const outfile = process.env.OUTFILE
 const timeLimit = +process.env.TIME_LIMIT
 
-run(url, { batchSize, headers, outfile, timeLimit })
+run(url, { batchSize, cookies, headers, outfile, timeLimit })
   .then(console.log)
   .catch(err => console.error(err.message) || 1)
   .then(process.exit)
