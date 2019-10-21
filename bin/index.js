09b31face72ef66f5e663a803fa907b16772e89d
zbo14 <zbalder68@gmail.com>
2019-10-20 23:12:00 -0400

    add URL to tree after visit, add logging, bump to node12, get list of paths from file

--- bin/index.js
+++ bin/index.js
@@ -2,16 +2,27 @@
 
 'use strict'
 
-const run = require('./run')
+const crawler = require('./web-tree-crawler')
 
 const url = process.argv[2]
-const batchSize = +process.env.BATCH_SIZE
-const cookies = process.env.COOKIES
-const headers = process.env.HEADERS
-const outfile = process.env.OUTFILE
-const timeLimit = +process.env.TIME_LIMIT
 
-run(url, { batchSize, cookies, headers, outfile, timeLimit })
-  .then(console.log)
+const { env } = process
+const batchSize = +(env.batchSize || env.b)
+const cookies = env.cookies || env.c
+const headers = env.headers || env.h
+const outFile = env.outFile || env.o
+const pathList = env.pathList || env.p
+const timeLimit = +env.timeLimit || env.t
+const verbose = (env.verbose || env.v || '').trim() === 'true'
+
+crawler(url, {
+  batchSize,
+  cookies,
+  headers,
+  outFile,
+  pathList
+  timeLimit,
+  verbose
+}).then(console.log)
   .catch(err => console.error(err.message) || 1)
   .then(process.exit)
---
+++ bin/web-tree-crawler.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const fs = require('fs').promises
+const crawl = require('../lib')
+
+const usage = [
+  'Usage: [OPTIONS=] web-tree-crawler URL\n',
+  'Options:',
+  '  batchSize , b   The number of requests to send at a time (default=200)',
+  '  cookies   , c   Cookies to send with each request',
+  '  headers   , h   Headers to send with each request',
+  '  outFile   , o   Write the tree to file instead of stdout',
+  '  pathList  , p   File containing paths to initially crawl',
+  '  timeLimit , t   The max number of seconds to run (default=120)',
+  '  verbose   , v   Log info and progress to stdout'
+].join('\n')
+
+module.exports = async (url, {
+  batchSize,
+  cookies,
+  headers,
+  outFile,
+  pathList,
+  timeLimit,
+  verbose
+} = {}) => {
+  if (!url) return usage
+
+  if (headers) {
+    headers = headers
+      .split(',')
+      .reduce((headers, header) => {
+        let [name, value] = header.split(':')
+        name = name && name.trim().toLowerCase()
+        value = value && value.trim()
+
+        if (name && value) {
+          headers[name] = value
+        }
+
+        return headers
+      }, {})
+  }
+
+  if (cookies) {
+    headers = headers || {}
+    headers.cookie = cookies
+      .split(';')
+      .map(cookie => cookie.trim())
+  }
+
+  let startPaths = ['robots.txt', 'sitemap.xml']
+
+  if (pathList) {
+    try {
+      const data = await fs.readFile(pathList, 'utf8')
+      startPaths = data.split('\n').filter(Boolean)
+    } catch (_) {
+      throw new Error('File not found: ' + pathList)
+    }
+  }
+
+  const result = await crawl(url, {
+    batchSize,
+    headers,
+    startPaths,
+    stringify: true,
+    timeLimit,
+    verbose
+  })
+
+  if (!outFile) return result
+
+  await fs.writeFile(outFile, result)
+
+  return 'Wrote tree to file!'
+}
